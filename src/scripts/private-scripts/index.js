a00121a040060136b8f2ff5a47c32771c3f945dd
GIang Vu <giangvpt@smartosc.com>
2023-09-27 11:32:52 +0700

    feat: [develop] add services slider section

--- src/scripts/private-scripts/index.js
+++ src/scripts/private-scripts/index.js
@@ -1,8 +1,9 @@
 $(document).ready(function () {
   console.log(123);
-  var $slider = $(".slider");
-  var $progressBar = $(".progress");
+  var $slider = $(".services-slider");
+  var $progressBar = $(".slider-progress");
   var $progressBarLabel = $(".slider__label");
+  var $serviceSliderButton = $(".service-slider__next-button");
 
   $slider.on("beforeChange", function (event, slick, currentSlide, nextSlide) {
     var calc = (nextSlide / (slick.slideCount - 1)) * 100;
@@ -14,5 +15,30 @@ $(document).ready(function () {
     $progressBarLabel.text(calc + "% completed");
   });
 
-  $slider.slick({ slidesToShow: 3, slidesToScroll: 1, speed: 400 });
+  $serviceSliderButton.on("click", function () {
+    console.log(123234234);
+    $slider.slick("slickNext");
+  });
+
+  $slider.slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    speed: 400,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 768, // Adjust this breakpoint as needed
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480, // Adjust this breakpoint as needed
+        settings: {
+          slidesToShow: 1,
+          centerMode: true
+        },
+      },
+    ],
+  });
 });
